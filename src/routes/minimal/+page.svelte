20d6677f438be65069ad7dafbeb01325b18ce764
AJ Caldwell <tomatrow@gmail.com>
2022-09-17 22:56:35 -0700

    Add minimal example

---
+++ src/routes/minimal/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Map } from "$lib"
+</script>
+
+<Map
+	class="map"
+	options={{
+		zoom: 5,
+		zoomControl: true,
+		center: { lat: 33.12736343851687, lng: -116.2786826846543 }
+	}}
+/>
+
+<style>
+	:global(.map) {
+		width: 100%;
+		height: 100%;
+	}
+</style>
